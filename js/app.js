1365c148d301c9c1281ce8ca2918c4ec07b8a04b
Matias Ricardo Carrillo Sanchez <matiascarrillo115@gmail.com>
2024-06-18 21:02:42 -0500

    Problema 4 completado, passes test

--- js/app.js
+++ js/app.js
@@ -67,15 +67,27 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4,]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    let Suma = 0;
+    // let Numbers = "";
+    for (let i = 0; i < sumArr.length; i++) {
+        // same as sumaAr = sumaAr + sumArr
+        Suma += sumArr[i];;
+        // Numbers += sumArr[i];
+        // como agrego las comas?
+        // if (i < sumArr.length) {
+        //   Numbers += ",";
+        // };
+        // no funciona*
+    }
+    let msgAr = sumArr[0]+","+sumArr[1]+","+sumArr[2] + " was passed in as an array of numbers, and "+ Suma +" is their sum.";
+    return[Suma,msgAr]
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
